2874419b6a71640382f2872c660d08a5a7c7d0fa
Dale Syth <dalesyth@gmail.com>
2023-05-12 14:12:12 -0500

    completed lab

--- client/app.js
+++ client/app.js
@@ -1,19 +1,21 @@
+import { voteTakeout } from "./score.js";
+import { getWinner } from "./score.js";
+import { voteLeftovers } from "./score.js";
 const winnerText = document.getElementById("winner");
 const leftoversBtn = document.getElementById("leftovers");
 const takeoutBtn = document.getElementById("takeout");
 
 const updateWinner = () => {
-	const currWinner = getWinner();
-	winnerText.innerText = currWinner;
+  const currWinner = getWinner();
+  winnerText.innerText = currWinner;
 };
 
 takeoutBtn.addEventListener("click", () => {
-	voteTakeout();
-	updateWinner();
+  voteTakeout();
+  updateWinner();
 });
 
 leftoversBtn.addEventListener("click", () => {
-	voteLeftovers();
-	updateWinner();
+  voteLeftovers();
+  updateWinner();
 });
-
